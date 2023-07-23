36868fde5de8e584083b5cdf204fc40fb50c6bd7
VeNickov <78678788+VeNickov@users.noreply.github.com>
2023-07-23 10:17:28 +0300

    Add files via upload

--- admin/template/js/ckeditor/plugins/colorbutton/lang/ua.js
+++ admin/template/js/ckeditor/plugins/colorbutton/lang/ua.js
@@ -1,8 +1,8 @@
-﻿/*
+/*
 Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
 */
-CKEDITOR.plugins.setLang( 'colorbutton', 'uk', {
+CKEDITOR.plugins.setLang( 'colorbutton', 'ua', {
 	auto: 'Авто',
 	bgColorTitle: 'Колір фону',
 	colors: {
